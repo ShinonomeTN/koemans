535fd972c3c1c96507332ff70672b679efbe7426
CattenLinger <cattenlinger@outlook.com>
2021-10-04 23:45:59 +0800

    Route injection now works with beans that provided by methods in configurations(see the test case). Added some comments.

--- koemans-web-spring/src/main/kotlin/com/shinonometn/koemans/web/spring/RouteInjection.kt
+++ koemans-web-spring/src/main/kotlin/com/shinonometn/koemans/web/spring/RouteInjection.kt
@@ -3,31 +3,53 @@ package com.shinonometn.koemans.web.spring
 import io.ktor.application.*
 import io.ktor.routing.*
 
-/*
-*
-* Provide Routing configuration features
-*
-*/
-
+/**
+ * Provide `routing{}` dsl configuration features.
+ */
 interface RoutingProvider {
     fun Routing.provide()
+
+    companion object {
+        operator fun invoke(body : Routing.() -> Unit) = object : RoutingProvider {
+            override fun Routing.provide() = body()
+        }
+    }
 }
 
-@RouteGroup
+/**
+ * Provide `route{}` dsl configuration features
+ */
 interface RouteProvider {
     fun Route.provide()
+
+    companion object {
+        operator fun invoke(body: Route.() -> Unit) = object : RouteProvider {
+            override fun Route.provide() = body()
+        }
+    }
 }
 
+/**
+ * Annotation for route groups.
+ * See [Route.injectRouteGroup].
+ */
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class RouteGroup(val name: String = "default")
 
+/**
+ * Apply route configurations that provided by type [T]
+ */
 inline fun <reified T : RouteProvider> Route.injectRoute() {
     val provider = application.springContext.find<T>()
     with(provider) { this@injectRoute.provide() }
     application.log.info("Installed route from '{}' on '{}'.", provider::class.simpleName, this.toString())
 }
 
+/**
+ * Apply route configurations by specified annotations, with an optional [selector] for
+ * annotation filtering.
+ */
 inline fun <reified T : Annotation> Route.injectRouteGroup(selector: (T) -> Boolean = { true }) {
     val providers = application.springContext.getBeansWithAnnotation(T::class.java).values.filterIsInstance<RouteProvider>().filter {
         selector(it::class.annotations.filterIsInstance<T>().first())
@@ -40,21 +62,45 @@ inline fun <reified T : Annotation> Route.injectRouteGroup(selector: (T) -> Bool
     application.log.info("Installed {} route provider(s) from group '@{}' on '{}'.", providers.size, T::class.simpleName, this.toString())
 }
 
+/**
+ * Apply route configurations by route groups that matches a given [name].
+ */
 fun Route.injectRouteGroup(name: String) {
-    val providers = application.springContext.getBeansWithAnnotation(RouteGroup::class.java).values.filterIsInstance<RouteProvider>().filter {
-        val annotations = it::class.annotations.filterIsInstance<RouteGroup>()
-        if (annotations.isEmpty()) false else annotations.first().name == name
+    val context = application.springContext
+    val log = application.log
+
+    // Find providers
+    val providers = context.getBeansWithAnnotation(RouteGroup::class.java).mapNotNull { e ->
+        when {
+            // Accept RouteProvider only
+            e.value !is RouteProvider -> null
+
+            // Given name matches the annotation on bean (when exists)
+            name != context.findAnnotationOnBean(e.key, RouteGroup::class.java)?.name -> null
+
+            // Return entry value
+            else -> e.value as RouteProvider
+        }
     }.takeUnless {
         it.isEmpty()
-    } ?: return application.log.info("No route installed on '{}' because no provider in group '{}'.", this.toString(), name)
+    } ?: return log.info("No route installed on '{}' because no provider in group '{}'.", this.toString(), name)
 
     providers.forEach { with(it) { this@injectRouteGroup.provide() } }
 
-    application.log.info("Installed {} route provider(s) from group '{}' on '{}'.", providers.size, name, this.toString())
+    log.info("Installed {} route provider(s) from group '{}' on '{}'.", providers.size, name, this.toString())
 }
 
+/**
+ * Automatically install routing configuration (usually a root routing configuration) by
+ * [RoutingProvider]s and [RouteProvider]s in Spring context.
+ *
+ * If has any bean name conflict, Spring will not load the later one (according to dependencies)
+ * with the same name.
+ */
 fun Routing.installSpringRoutingConfigurations() {
-    val configs = application.springContext.getBeansOfType(RoutingProvider::class.java).values.takeUnless {
+    val context = application.springContext
+
+    val configs = context.getBeansOfType(RoutingProvider::class.java).values.takeUnless {
         it.isEmpty()
     } ?: return application.log.warn("No routing configurations.")
 
--- koemans-web-spring/src/test/kotlin/com/shinonometn/koemans/web/spring/SpringContextTest.kt
+++ koemans-web-spring/src/test/kotlin/com/shinonometn/koemans/web/spring/SpringContextTest.kt
@@ -2,11 +2,55 @@ package com.shinonometn.koemans.web.spring
 
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.testing.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+
+@ComponentScan("com.shinonometn.koemans.web.spring")
+open class TestApplicationAutoConfiguration {
+
+    @Value("\${application.title}")
+    lateinit var applicationTitle: String
+        private set
+
+    @Bean
+    open fun rootRouting() = RoutingProvider {
+        injectRouteGroup("default_routing_group")
+        injectRoute<TestRouteConfigClass>()
+    }
+}
+
+@Configuration
+open class TestRouteInjectionConfiguration {
+
+    @Bean
+    @RouteGroup("default_routing_group")
+    open fun indexRoute() = RouteProvider {
+        get("/group") {
+            call.respondText { "Installed from default route group." }
+        }
+    }
+
+}
+
+@Component
+class TestRouteConfigClass(private val config: TestApplicationAutoConfiguration) : RouteProvider {
+    override fun Route.provide() {
+        get { call.respondText { "Hello world" } }
+
+        get("/property/title") {
+            call.respondText { "Application Title: ${config.applicationTitle}" }
+        }
+    }
+}
 
 fun Application.mainTestModule() {
     install(SpringContext) {
@@ -25,6 +69,13 @@ class SpringContextTest {
     @Test
     fun `Test context load`() {
         withTestApplication(Application::mainTestModule) {
+            application.springContext
+        }
+    }
+
+    @Test
+    fun `Test basic request`() {
+        withTestApplication(Application::mainTestModule) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello world", response.content)
@@ -33,9 +84,19 @@ class SpringContextTest {
     }
 
     @Test
+    fun `Test route group install`() {
+        withTestApplication(Application::mainTestModule) {
+            handleRequest(HttpMethod.Get, "/group").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Installed from default route group.", response.content)
+            }
+        }
+    }
+
+    @Test
     fun `Test context property load`() {
         withTestApplication(Application::mainTestModule) {
-            handleRequest(HttpMethod.Get, "/property").apply {
+            handleRequest(HttpMethod.Get, "/property/title").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Application Title: Hello world app", response.content)
             }
