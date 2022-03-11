42e26faf6f1019177e44fa4011a881253f8377bf
CattenLinger <cattenlinger@outlook.com>
2022-03-11 20:51:11 +0800

    Fix bug and adding test cases for FilterQuerySupport

--- koemans-exposed/src/main/kotlin/com/shinonometn/koemans/exposed/FilteringQuerySupport.kt
+++ koemans-exposed/src/main/kotlin/com/shinonometn/koemans/exposed/FilteringQuerySupport.kt
@@ -4,29 +4,41 @@ import org.jetbrains.exposed.sql.*
 
 typealias PredicateFragmentBuilder = SqlExpressionBuilder.(FilterOptionMapping.ValueWrapper) -> Op<Boolean>
 
-class FilterRequest internal constructor(internal var op : Op<Boolean>, val parameters : List<String>) {
-    fun append(predicate : SqlExpressionBuilder.() -> Op<Boolean>) : Op<Boolean> {
+class FilterRequest internal constructor(internal var op: Op<Boolean>, val parameters: List<String>) {
+    fun append(predicate: SqlExpressionBuilder.() -> Op<Boolean>): Op<Boolean> {
         op = op.and(predicate)
         return op
     }
 }
 
-fun FieldSet.selectBy(filterRequest: FilterRequest, additionalBuilder : (SqlExpressionBuilder.(Op<Boolean>) -> Op<Boolean>)? = null) : Query {
-    if(filterRequest.parameters.isEmpty()) return selectAll()
-    return additionalBuilder?.let { select { additionalBuilder(this, filterRequest.op)} } ?: select(filterRequest.op)
+fun FieldSet.selectBy(filterRequest: FilterRequest, additionalBuilder: (SqlExpressionBuilder.(Op<Boolean>) -> Op<Boolean>)? = null): Query {
+
+    // When no params in filter, use the additional builder.
+    // If no provided builder, just return selectAll()
+    if (filterRequest.parameters.isEmpty()) return when (additionalBuilder) {
+        null -> selectAll()
+        else -> select { additionalBuilder(this, Op.TRUE) }
+    }
+
+    // When there is no additional builder, use the filterRequest.op
+    // If not, combine the filterRequest.op and the exp provided by additional builder
+    return when(additionalBuilder) {
+        null -> select { filterRequest.op }
+        else -> select { additionalBuilder(this, filterRequest.op) }
+    }
 }
 
 typealias FilterParams = Map<String, List<String>?>
 
-class FilterOptionMapping internal constructor(val config : Configuration){
+class FilterOptionMapping internal constructor(val config: Configuration) {
 
-    constructor(builder : Configuration.() -> Unit) : this(Configuration()) {
+    constructor(builder: Configuration.() -> Unit) : this(Configuration()) {
         config.builder()
     }
 
-    operator fun invoke(params : FilterParams) = buildFilterRequest(params)
+    operator fun invoke(params: FilterParams) = buildFilterRequest(params)
 
-    fun buildFilterRequest(params : FilterParams): FilterRequest {
+    private fun buildFilterRequest(params: FilterParams): FilterRequest {
         val keys = config.mapping.keys.filter {
             val param = params[it]
             param != null && param.isNotEmpty()
@@ -36,38 +48,39 @@ class FilterOptionMapping internal constructor(val config : Configuration){
             config.mapping[it]!!.invoke(SqlExpressionBuilder, ValueWrapper(it, params))
         }
 
-        val op = if(fragments.isNotEmpty()) Op.TRUE else // Fix problem with empty filter
-            config.expression(SqlExpressionBuilder, fragments)
+        val op = if (fragments.isEmpty()) Op.TRUE else // Fix problem with empty filter
+            config.opBuilder(SqlExpressionBuilder, fragments)
 
         return FilterRequest(op, keys)
     }
 
-    class ValueWrapper(val name : String, private val parameters : FilterParams) {
+    class ValueWrapper(val name: String, private val parameters: FilterParams) {
         fun asString() = parameters[name]!!.first()
-        fun asList() : List<String> = parameters[name]!!
+        fun asList(): List<String> = parameters[name]!!
+        fun <T> convertTo(converter : (List<String>) -> T) = converter(parameters[name]!!)
     }
 
     class Configuration {
         internal val mapping = LinkedHashMap<String, PredicateFragmentBuilder>()
 
-        var expression : SqlExpressionBuilder.(Map<String, Op<Boolean>>) -> Op<Boolean> = {
+        var opBuilder: SqlExpressionBuilder.(Map<String, Op<Boolean>>) -> Op<Boolean> = {
             AndOp(it.values.toList())
         }
 
-        infix fun String.means(builder : PredicateFragmentBuilder) : Pair<String, PredicateFragmentBuilder> {
+        infix fun String.means(builder: PredicateFragmentBuilder): Pair<String, PredicateFragmentBuilder> {
             mapping[this] = builder
             return this to builder
         }
 
-        fun exclude(vararg name : String) {
+        fun exclude(vararg name: String) {
             name.forEach { mapping.remove(it) }
         }
     }
 
-    fun copy(builder : Configuration.() -> Unit) : FilterOptionMapping {
+    fun copy(builder: Configuration.() -> Unit): FilterOptionMapping {
         val newConfig = Configuration()
         newConfig.mapping.putAll(config.mapping)
-        newConfig.expression = config.expression
+        newConfig.opBuilder = config.opBuilder
         newConfig.builder()
         return FilterOptionMapping(newConfig)
     }
---
+++ koemans-exposed/src/test/kotlin/com/shinonometn/koemans/exposed/FilterQuerySupportTest.kt
@@ -0,0 +1,185 @@
+package com.shinonometn.koemans.exposed
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class FilterQuerySupportTest {
+    private val database = Database.connect(HikariDataSource(HikariConfig().apply {
+        jdbcUrl = "jdbc:sqlite:file:test_filter_query_support?mode=memory"
+        maximumPoolSize = 1
+    }))
+
+    object TestTable : IntIdTable("test_table") {
+        const val DDL = """
+            create table if not exists test_table(
+                id integer primary key,
+                name varchar(255),
+                age integer
+            )
+        """
+
+        val name = varchar("name", 255)
+        val age = integer("age")
+    }
+
+    @Before
+    fun setup() {
+        transaction(database) {
+            exec(TestTable.DDL)
+            TestTable.deleteAll()
+
+            listOf(
+                "alice" to 20, "bob" to 30, "charlie" to 40, "david" to 50,
+                "edward" to 60, "frank" to 70, "george" to 80, "harry" to 90,
+                "ian" to 90, "james" to 100, "kate" to 110, "lisa" to 120,
+            ).forEach { (name, age) ->
+                TestTable.insert {
+                    it[TestTable.name] = name
+                    it[TestTable.age] = age
+                }
+            }
+        }
+    }
+
+    val optionMappingForMatching = FilterOptionMapping {
+        "name" means { TestTable.name eq it.asString() }
+        "age" means { TestTable.age eq it.asString().toInt() }
+    }
+
+    @Test
+    fun `Test filter build`() {
+        val filter = optionMappingForMatching(
+            mapOf(
+                "name" to listOf("test"),
+                "age" to listOf("10")
+            )
+        )
+
+        println(filter.parameters.joinToString(",") { it })
+    }
+
+    @Test
+    fun `Test filter query none match`() {
+        val filter = optionMappingForMatching(
+            mapOf(
+                "name" to listOf("test"),
+                "age" to listOf("10")
+            )
+        )
+
+        val result = transaction(database) {
+            TestTable.selectBy(filter).map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(0, result.size)
+    }
+
+    @Test
+    fun `Test filer query match result`() {
+        val filter = optionMappingForMatching(
+            mapOf(
+                "name" to listOf("alice"),
+                "age" to listOf("20")
+            )
+        )
+
+        val result = transaction(database) {
+            TestTable.selectBy(filter).map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(1, result.size)
+        assertEquals("alice", result[0].first)
+        assertEquals(20, result[0].second)
+    }
+
+    val optionMappingForRange = FilterOptionMapping {
+        "age_start" means { TestTable.age greaterEq it.asString().toInt() }
+        "age_end" means { TestTable.age lessEq it.asString().toInt() }
+    }
+
+    @Test
+    fun `Test filter query range matched`() {
+        val filter = optionMappingForRange(
+            mapOf(
+                "age_start" to listOf("40"),
+                "age_end" to listOf("70")
+            )
+        )
+
+        val result = transaction(database) {
+            TestTable.selectBy(filter).map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(4, result.size)
+    }
+
+    @Test
+    fun `Test with additional query match`() {
+        val filter = optionMappingForRange(
+            mapOf(
+                "age_start" to listOf("20"),
+                "age_end" to listOf("70")
+            )
+        )
+
+        val result = transaction(database) {
+            TestTable.selectBy(filter) { it and (TestTable.name eq "edward") }.map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(1, result.size)
+        assertEquals("edward", result[0].first)
+    }
+
+    @Test
+    fun `Test only additional query matched`() {
+        val filter = optionMappingForRange(emptyMap())
+
+        val result = transaction(database) {
+            TestTable.selectBy(filter) { it and (TestTable.name eq "george") }.map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(1, result.size)
+        assertEquals("george", result[0].first)
+    }
+
+    val mappingForOrOp = FilterOptionMapping {
+        opBuilder = { OrOp(it.values.toList()) }
+        "name" means { TestTable.name eq it.asString() }
+        "age" means { TestTable.age eq it.asString().toInt() }
+    }
+
+    @Test
+    fun `Test or op`() {
+        val filtering = mappingForOrOp(mapOf(
+            "name" to listOf("harry"),
+            "age" to listOf("90")
+        ))
+
+        val result = transaction(database) {
+            TestTable.selectBy(filtering).map {
+                it[TestTable.name] to it[TestTable.age]
+            }.toList()
+        }
+
+        assertEquals(2, result.size)
+        assertTrue(result.map { it.first }.containsAll(listOf("harry", "ian")))
+    }
+
+}
