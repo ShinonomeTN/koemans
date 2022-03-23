1525c9b88c7ff825af76d47e6a581d515a4b6887
CattenLinger <cattenlinger@outlook.com>
2022-03-23 22:59:55 +0800

    Commit

--- koemans-exposed/src/main/kotlin/com/shinonometn/koemans/exposed/QueryEx.kt
+++ koemans-exposed/src/main/kotlin/com/shinonometn/koemans/exposed/QueryEx.kt
@@ -3,13 +3,15 @@ package com.shinonometn.koemans.exposed
 import org.jetbrains.exposed.sql.*
 
 fun <R, T : Table> T.countBy(column: Column<R>, predicate : (SqlExpressionBuilder.(T) -> Op<Boolean>)? = null) : Long {
-    return slice(column.count()).let { fs ->
+    val colCount = column.count()
+    return slice(colCount).let { fs ->
         predicate?.let { fs.select { predicate(this@countBy) } } ?: fs.selectAll()
-    }.map { it[column.count()] }.first()
+    }.map { it[colCount] }.first()
 }
 
 fun <R, T : Table> T.sumBy(column: Column<R>, predicate: (SqlExpressionBuilder.(T) -> Op<Boolean>)? = null) : R? {
-    return slice(column.sum()).let { fs ->
+    val colSum = column.sum()
+    return slice(colSum).let { fs ->
         predicate?.let { fs.select { predicate(this@sumBy) } } ?: fs.selectAll()
-    }.map { it[column.sum()] }.firstOrNull()
+    }.map { it[colSum] }.firstOrNull()
 }
