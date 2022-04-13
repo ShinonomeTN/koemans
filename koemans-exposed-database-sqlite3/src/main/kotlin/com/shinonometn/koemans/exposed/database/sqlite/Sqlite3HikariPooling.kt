977a6554be29ceb9c8b877c8385ca25497dcb115
CattenLinger <cattenlinger@outlook.com>
2022-04-13 16:07:55 +0800

    Allow HikariDatasource default config be overwritten

--- koemans-exposed-database-sqlite3/src/main/kotlin/com/shinonometn/koemans/exposed/database/sqlite/Sqlite3HikariPooling.kt
+++ koemans-exposed-database-sqlite3/src/main/kotlin/com/shinonometn/koemans/exposed/database/sqlite/Sqlite3HikariPooling.kt
@@ -38,10 +38,16 @@ val Sqlite3.Configuration.HikariPooling by lazy<Sqlite3.Configuration.() -> Data
 
 @Suppress("unused")
 fun Sqlite3.Configuration.HikariDatasource(hikariConfigurator: (HikariConfig) -> Unit) : Sqlite3.Configuration.(String) -> DataSource = {
-    val hikariConfig = HikariConfig().apply(hikariConfigurator)
+    val hikariConfig = HikariConfig().apply {
+        poolName = "Hikari-Sqlite3-${name}"
+        driverClassName = Sqlite3.DriverClassName
+        jdbcUrl = it
+        minimumIdle = 1
+        maximumPoolSize = 1
+    }
 
-    hikariConfig.jdbcUrl = it
-    hikariConfig.driverClassName = Sqlite3.DriverClassName
+    // Apply config overwrite
+    hikariConfig.apply(hikariConfigurator)
 
     HikariDataSource(hikariConfig)
 }
