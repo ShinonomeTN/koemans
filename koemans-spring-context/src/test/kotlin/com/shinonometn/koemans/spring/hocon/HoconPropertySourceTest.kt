42e29b7a784947af51f76214035be8edb500c2f0
CattenLinger <cattenlinger@outlook.com>
2022-04-21 22:04:28 +0800

    Commit

--- koemans-spring-context/src/test/kotlin/com/shinonometn/koemans/spring/hocon/HoconPropertySourceTest.kt
+++ koemans-spring-context/src/test/kotlin/com/shinonometn/koemans/spring/hocon/HoconPropertySourceTest.kt
@@ -3,14 +3,15 @@ package com.shinonometn.koemans.spring.hocon
 import com.shinonometn.koemans.spring.HoconPropertySource
 import com.typesafe.config.ConfigFactory
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class HoconPropertySourceTest {
     @Test
     fun `Test read`() {
         val properties = ConfigFactory.parseResources("hocon.conf").resolve()
-        val source = HoconPropertySource.buildPropertySourceFrom("null", properties)
-        Assert.assertEquals(source.getProperty("test.value1"), 86400)
-        Assert.assertEquals(source.getProperty("test.value2"), 86400)
+        val source = HoconPropertySource.buildPropertySourceFrom("hocon", properties)
+        assertEquals(86400, source.getProperty("test.value1"))
+        assertEquals(86400, source.getProperty("test.value2"))
     }
 }
