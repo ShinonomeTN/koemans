443dafc1326316f49dc33d98e5f72c238125e551
CattenLinger <cattenlinger@outlook.com>
2022-07-11 23:11:35 +0800

    Added ExceptionUtils

--- koemans-utils/src/main/kotlin/com/shinonometn/koemans/utils/ExceptionUtils.kt
+++ koemans-utils/src/main/kotlin/com/shinonometn/koemans/utils/ExceptionUtils.kt
@@ -1,19 +1,34 @@
 package com.shinonometn.koemans.utils
 
+/**
+ * Get stack trace strings that matching predicate
+ */
 fun Exception.stackTraceLines(filter : (StackTraceElement) -> Boolean) : List<String> {
     val stackTraceElements = stackTrace ?: return emptyList()
     return stackTraceElements.filter(filter).map { it.simpleStringRepresent() }
 }
 
+/**
+ * Get stack trace strings
+ */
 fun Exception.stackTraceLines() = stackTraceLines { true }
 
-fun Exception.stackTraceLines(count : Int) : List<String> {
+/**
+ * Get stack trace strings in given deep
+ */
+fun Exception.stackTraceLines(deep : Int) : List<String> {
     val stackTraceElement = stackTrace ?: return emptyList()
-    return stackTraceElement.take(count).map { it.simpleStringRepresent() }
+    return stackTraceElement.take(deep).map { it.simpleStringRepresent() }
 }
 
+/**
+ * Get stack trace string from StackTraceElement
+ */
 fun StackTraceElement.simpleStringRepresent() = "${className}.${methodName}@${fileName}:${lineNumber}"
 
+/**
+ * Get first stack trace element string
+ */
 fun Exception.firstCauseString() : String? {
     return stackTrace.firstOrNull()?.simpleStringRepresent()
 }
