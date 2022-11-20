4700bdceaa269715b89dccbb696503fae9f59782
cattenlinger <lingerkong03@gmail.com>
2022-11-20 13:17:47 +0800

    Improve apis

--- koemans-utils/src/main/kotlin/com/shinonometn/koemans/utils/StringUtils.kt
+++ koemans-utils/src/main/kotlin/com/shinonometn/koemans/utils/StringUtils.kt
@@ -4,7 +4,6 @@ import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
-import kotlin.time.Duration
 
 /**
  * Copy from io.ktor.server.engine.CommandLine
@@ -95,9 +94,6 @@ fun String.isDecimal(maxFloatDigests: Int = 18, allowLeftPaddingZeros: Boolean =
     }
 }
 
-
-/** Parse a duration from string. Returns a kotlin duration */
-fun String.toDuration() = Duration.parse(this)
 
 /**
  * Check if string is a literal boolean
---
+++ koemans-utils/src/main/kotlin/com/shinonometn/koemans/utils/TimeUtils.kt
@@ -0,0 +1,20 @@
+package com.shinonometn.koemans.utils
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.time.Duration
+
+/** Parse a duration from string. Returns a kotlin duration */
+fun String.toDuration() = Duration.parse(this)
+
+private val defaultLocalDateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME
+
+/** Parse string using ISO_DATE_TIME format */
+fun String.toLocalDateTime(formatter: DateTimeFormatter = defaultLocalDateTimeFormatter): LocalDateTime = LocalDateTime.parse(this, formatter)
+
+/** Parse string using ISO_DATE_TIME format or null if it's invalid */
+fun String.toLocalDateTimeOrNull(formatter: DateTimeFormatter = defaultLocalDateTimeFormatter): LocalDateTime? = try {
+    LocalDateTime.parse(this, formatter)
+} catch (ignored: Exception) {
+    null
+}
